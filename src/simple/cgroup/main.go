f794afa1d41fa00ab6f6246b41f8ce659500c3a2
ZhihaoQuinn <howzequinn@gmail.com>
2020-09-03 00:43:25 +0800

    feat(Cgroup): coding for cgroup of linux
    Add coding for cgroup of linux.

---
+++ src/simple/cgroup/main.go
@@ -0,0 +1,55 @@
+// +build linux
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+// 挂载了memory subsystem的hierarchy的根目录
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+func main() {
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Printf("current container pid is %d .\n", syscall.Getpid())
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	cmd := exec.Command("/proc/self/exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	} else {
+		// 映射出进程fork出的子进程在外部命名空间的Pid
+		fmt.Printf("%v", cmd.Process.Pid)
+
+		// 在`挂载了memory subsystem的hierarchy的根目录`上创建新的cgroup
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), os.FileMode(0755).Perm())
+		// 将容器进程放进此cgroup中
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), os.FileMode(0644).Perm())
+		// 限制进程memory
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"), os.FileMode(0644).Perm())
+
+		cmd.Process.Wait()
+	}
+}
