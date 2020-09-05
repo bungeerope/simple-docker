5c5a097de85bc30c3cb5e7167505fb588d7eeabd
ZhihaoQuinn <howzequinn@gmail.com>
2020-09-05 23:49:58 +0800

    feat(subsystem): add subsystem
    add subsystem files

--- src/docker/subsystem/cpu.go
+++ src/docker/subsystem/cpu.go
@@ -2,3 +2,19 @@ package subsystem
 
 type CpuSubsystem struct {
 }
+
+func (c *CpuSubsystem) Name() string {
+	panic("implement me")
+}
+
+func (c *CpuSubsystem) Set(path string, res *ResourceConfig) error {
+	panic("implement me")
+}
+
+func (c *CpuSubsystem) Apply(path string, pid int) error {
+	panic("implement me")
+}
+
+func (c *CpuSubsystem) Remove(path string) error {
+	panic("implement me")
+}
--- src/docker/subsystem/cpuset.go
+++ src/docker/subsystem/cpuset.go
@@ -2,3 +2,19 @@ package subsystem
 
 type CpusetSubsystem struct {
 }
+
+func (c *CpusetSubsystem) Name() string {
+	panic("implement me")
+}
+
+func (c *CpusetSubsystem) Set(path string, res *ResourceConfig) error {
+	panic("implement me")
+}
+
+func (c *CpusetSubsystem) Apply(path string, pid int) error {
+	panic("implement me")
+}
+
+func (c *CpusetSubsystem) Remove(path string) error {
+	panic("implement me")
+}
